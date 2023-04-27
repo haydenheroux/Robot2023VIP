08fb50c6fdfa0a44a9abdb8f181ea963d6f45eb8
Hayden Heroux <haydenheroux@gmail.com>
2023-04-27 03:58:40 -0400

    refactor(Claw): Feed in simulated currents from NetworkTables.

--- src/main/java/frc/robot/intake/ClawIOSim.java
+++ src/main/java/frc/robot/intake/ClawIOSim.java
@@ -4,16 +4,41 @@
 
 package frc.robot.intake;
 
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.networktables.BooleanTopic;
+import edu.wpi.first.networktables.DoubleTopic;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 public class ClawIOSim implements ClawIO {
 
     private double simMotorCurrent; 
 
-    public boolean isHoldingCone, isHoldingCube;
-    public double holdingConeCurrent, holdingCubeCurrent;
+    public final BooleanSupplier isHoldingCone, isHoldingCube;
+    public final DoubleSupplier holdingConeCurrent, holdingCubeCurrent;
 
     private final double kResistance = 1.0;
 
     public ClawIOSim() {
+      NetworkTable clawSim = NetworkTableInstance.getDefault().getTable("clawSim");
+
+      BooleanTopic _isHoldingCone = clawSim.getBooleanTopic("isHoldingCone");
+      _isHoldingCone.publish().setDefault(false);
+      isHoldingCone = clawSim.getBooleanTopic("isHoldingCone").subscribe(false);
+
+      BooleanTopic _isHoldingCube = clawSim.getBooleanTopic("isHoldingCube");
+      _isHoldingCube.publish().setDefault(false);
+      isHoldingCube = clawSim.getBooleanTopic("isHoldingCube").subscribe(false);
+
+      DoubleTopic _holdingConeCurrent = clawSim.getDoubleTopic("holdingConeCurrent");
+      _holdingConeCurrent.publish().setDefault(0.0);
+      holdingConeCurrent = clawSim.getDoubleTopic("holdingConeCurrent").subscribe(0.0);
+
+      DoubleTopic _holdingCubeCurrent = clawSim.getDoubleTopic("holdingCubeCurrent");
+      _holdingCubeCurrent.publish().setDefault(0.0);
+      holdingCubeCurrent = clawSim.getDoubleTopic("holdingCubeCurrent").subscribe(0.0);
     }
 
   @Override
@@ -26,10 +51,12 @@ public class ClawIOSim implements ClawIO {
 
   @Override
   public void setMotorVoltage(double volts) {
-    if (isHoldingCone) {
-        simMotorCurrent = holdingConeCurrent;
-    } else if (isHoldingCube) {
-        simMotorCurrent = holdingCubeCurrent;
+    if (volts == 0.0) {
+        simMotorCurrent = 0.0;
+    } else if (isHoldingCone.getAsBoolean()) {
+        simMotorCurrent = holdingConeCurrent.getAsDouble();
+    } else if (isHoldingCube.getAsBoolean()) {
+        simMotorCurrent = holdingCubeCurrent.getAsDouble();
     } else {
         simMotorCurrent = volts * kResistance;
     }
