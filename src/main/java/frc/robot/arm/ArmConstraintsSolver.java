950e844b450c0090f5ffd900916448b851f27a20
Hayden Heroux <haydenheroux@gmail.com>
2023-04-29 14:25:34 -0400

    refactor: Rewrite ArmConstraintsSolver.

--- src/main/java/frc/robot/arm/ArmConstraintsSolver.java
+++ src/main/java/frc/robot/arm/ArmConstraintsSolver.java
@@ -4,37 +4,45 @@
 
 package frc.robot.arm;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import frc.robot.Constants;
 
 public class ArmConstraintsSolver {
 
-  public static boolean isBelow(
-      double totalLengthMeters, double totalAngleRadians, double maxHeightMeters) {
-    double armHeightMeters =
-        totalLengthMeters * Math.sin(totalAngleRadians) + Constants.Arm.Constraints.HEIGHT_OFFSET;
-    return armHeightMeters < maxHeightMeters;
+  private static double calculateHorizontal(double distanceMeters, double angleRadians) {
+    double cos = Math.cos(angleRadians);
+    if (cos == 0) {
+      return Double.POSITIVE_INFINITY;
+    }
+
+    return distanceMeters / cos;
   }
 
-  public static boolean isAbove(
-      double totalLengthMeters, double totalAngleRadians, double minHeightMeters) {
-    double armHeightMeters =
-        totalLengthMeters * Math.sin(totalAngleRadians) + Constants.Arm.Constraints.HEIGHT_OFFSET;
-    return armHeightMeters > minHeightMeters;
+  private static double calculateVertical(double distanceMeters, double angleRadians) {
+    double sin = Math.sin(angleRadians);
+    if (sin == 0) {
+      return Double.POSITIVE_INFINITY;
+    }
+
+    return distanceMeters / sin;
   }
 
-  public static boolean isOutLessThan(
-      double totalLengthMeters, double totalAngleRadians, double maxLengthMeters) {
-    double armLengthMeters = totalLengthMeters * Math.cos(totalAngleRadians);
-    return armLengthMeters < maxLengthMeters;
-  }
+  public static double calculateMaximumExtensionLength(double angleRadians) {
+    ArrayList<Double> maximumLengths = new ArrayList<Double>();
+
+    maximumLengths.add(calculateHorizontal(Constants.Arm.Constraints.MAX_OUT_LENGTH, angleRadians));
+    maximumLengths.add(calculateVertical(Constants.Arm.Constraints.MIN_HEIGHT - Constants.Arm.Constraints.HEIGHT_OFFSET, angleRadians));
+    maximumLengths.add(calculateVertical(Constants.Arm.Constraints.MAX_HEIGHT - Constants.Arm.Constraints.HEIGHT_OFFSET, angleRadians));
+
+    maximumLengths.removeIf(x -> x < 0);
+
+    return Collections.min(maximumLengths) - Constants.Arm.Constraints.LENGTH_OFFSET;
+  } 
 
   public static boolean isWithinRuleZone(ArmPosition position) {
-    double totalLengthMeters =
-        position.extensionLengthMeters + Constants.Arm.Constraints.LENGTH_OFFSET;
-    double totalAngleRadians = position.rotationAngleRadians;
-    return isBelow(totalLengthMeters, totalAngleRadians, Constants.Arm.Constraints.MAX_HEIGHT)
-        && isAbove(totalLengthMeters, totalAngleRadians, Constants.Arm.Constraints.MIN_HEIGHT)
-        && isOutLessThan(
-            totalLengthMeters, totalAngleRadians, Constants.Arm.Constraints.MAX_OUT_LENGTH);
+    double maximumExtensionLength = calculateMaximumExtensionLength(position.rotationAngleRadians);
+    return position.extensionLengthMeters < maximumExtensionLength;
   }
 }
