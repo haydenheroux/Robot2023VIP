d81e9af5e5290dc839faf6897882121aa042e543
Hayden Heroux <haydenheroux@gmail.com>
2023-09-22 13:02:37 -0400

    chore: Run `spotlessApply`.

--- src/main/java/frc/robot/swerve/ModuleIO.java
+++ src/main/java/frc/robot/swerve/ModuleIO.java
@@ -5,38 +5,38 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public interface ModuleIO {
 
-    /**
-     * Sets the module setpoint.
-     * 
-     * @param setpoint the setpoint of the module.
-     */
-    public void setSetpoint(SwerveModuleState setpoint);
+  /**
+   * Sets the module setpoint.
+   *
+   * @param setpoint the setpoint of the module.
+   */
+  public void setSetpoint(SwerveModuleState setpoint);
 
-    /**
-     * Returns true if the module is at the setpoint.
-     * 
-     * @return
-     */
-    public boolean atSetpoint();
+  /**
+   * Returns true if the module is at the setpoint.
+   *
+   * @return
+   */
+  public boolean atSetpoint();
 
-    /**
-     * Gets the most recently set setpoint
-     *
-     * @return the most recently set setpoint.
-     */
-    public SwerveModuleState getSetpoint(); 
+  /**
+   * Gets the most recently set setpoint
+   *
+   * @return the most recently set setpoint.
+   */
+  public SwerveModuleState getSetpoint();
 
-    /**
-     * Gets the current state of the module.
-     *
-     * @return the current state of the module.
-     */
-    public SwerveModuleState getState();
+  /**
+   * Gets the current state of the module.
+   *
+   * @return the current state of the module.
+   */
+  public SwerveModuleState getState();
 
-    /**
-     * Gets the current position of the module.
-     *
-     * @return the current position of the module.
-     */
-    public SwerveModulePosition getPosition();
+  /**
+   * Gets the current position of the module.
+   *
+   * @return the current position of the module.
+   */
+  public SwerveModulePosition getPosition();
 }
