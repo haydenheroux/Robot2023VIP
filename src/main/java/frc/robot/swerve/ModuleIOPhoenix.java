7d2fe6111d951697e51c91b1e99dfef30b4c9515
Hayden Heroux <haydenheroux@gmail.com>
2023-09-23 17:03:34 -0400

    fix(Swerve): Configure Phoenix-created modules.

--- src/main/java/frc/robot/swerve/ModuleIOPhoenix.java
+++ src/main/java/frc/robot/swerve/ModuleIOPhoenix.java
@@ -3,6 +3,8 @@ package frc.robot.swerve;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.lib.hardware.ConfigurationApplier;
+import frc.robot.Constants.Swerve;
 
 public class ModuleIOPhoenix implements ModuleIO {
 
@@ -12,6 +14,10 @@ public class ModuleIOPhoenix implements ModuleIO {
 
   public ModuleIOPhoenix(ModuleConfiguration config) {
     module = new SwerveModule(config.getSwerveModuleConstants(), config.can.bus, true);
+
+    ConfigurationApplier.apply(Swerve.AZIMUTH_CONFIG, module.getCANcoder());
+    ConfigurationApplier.apply(Swerve.DRIVE_CONFIG, module.getDriveMotor());
+    ConfigurationApplier.apply(Swerve.STEER_CONFIG, module.getSteerMotor());
   }
 
   @Override
