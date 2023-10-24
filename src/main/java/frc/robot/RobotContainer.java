8af70dc950d2bbb542b5351d6adbe500dd975705
Hayden Heroux <haydenheroux@gmail.com>
2023-10-24 18:45:16 -0400

    refactor: Add CustomXboxController.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,14 +2,11 @@ package frc.robot;
 
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.lib.CustomXboxController;
 import frc.lib.mechanism.Mechanisms;
 import frc.lib.telemetry.TelemetryManager;
 import frc.robot.arm.Arm;
@@ -20,25 +17,22 @@ import frc.robot.intake.SideIntake;
 import frc.robot.odometry.Odometry;
 import frc.robot.swerve.Drive;
 import frc.robot.swerve.Swerve;
-import java.util.function.DoubleSupplier;
 
 public class RobotContainer {
   private final Arm arm;
-  // private final Compressor compressor;
   private final Claw claw;
   private final SideIntake sideIntake;
   private final Swerve swerve;
   private final Odometry odometry;
 
-  private final CommandXboxController driver = new CommandXboxController(0);
-  private final CommandXboxController operator = new CommandXboxController(1);
+  private final CustomXboxController driver = new CustomXboxController(0);
+  private final CustomXboxController operator = new CustomXboxController(1);
 
   private final SendableChooser<PathPlannerTrajectory> pathChooser =
       new SendableChooser<PathPlannerTrajectory>();
 
   public RobotContainer() {
     arm = Arm.getInstance();
-    // compressor = new Compressor(Ports.PNEUMATICS_MODULE, Ports.PNEUMATICS_MODULE_TYPE);
     claw = Claw.getInstance();
     sideIntake = SideIntake.getInstance();
     swerve = Swerve.getInstance();
@@ -71,16 +65,8 @@ public class RobotContainer {
     driver.x().whileTrue(swerve.cross());
     driver.y().whileTrue(new PrintCommand("TODO"));
 
-    DoubleSupplier extensionAxis =
-        () -> MathUtil.applyDeadband(-operator.getRawAxis(XboxController.Axis.kRightY.value), 0.1);
-    DoubleSupplier rotationAxis =
-        () -> MathUtil.applyDeadband(-operator.getRawAxis(XboxController.Axis.kLeftY.value), 0.1);
-
-    Trigger shouldExtend = new Trigger(() -> extensionAxis.getAsDouble() != 0);
-    Trigger shouldRotate = new Trigger(() -> rotationAxis.getAsDouble() != 0);
-
-    shouldExtend.whileTrue(arm.manualExtend(extensionAxis));
-    shouldRotate.whileTrue(arm.manualRotate(rotationAxis));
+    operator.leftY().whileTrue(arm.manualRotate(operator::getLeftY));
+    operator.rightY().whileTrue(arm.manualExtend(operator::getRightY));
 
     operator.a().whileTrue(arm.toGoal(ArmPosition.SCORE_L2));
     operator.b().whileTrue(arm.toGoal(ArmPosition.SCORE_L3));
@@ -92,11 +78,6 @@ public class RobotContainer {
 
     operator.leftBumper().onTrue(sideIntake.accept()).onFalse(sideIntake.holdOrDisable());
     operator.rightBumper().onTrue(sideIntake.eject()).onFalse(sideIntake.disable());
-
-    /*     operator
-    .start()
-    .onTrue(Commands.runOnce(compressor::enableDigital))
-    .onFalse(Commands.runOnce(compressor::disable)); */
   }
 
   /** Configures default commands for each subsystem. */
@@ -104,10 +85,10 @@ public class RobotContainer {
     swerve.setDefaultCommand(
         new Drive(
             swerve,
-            () -> MathUtil.applyDeadband(-driver.getLeftY(), 0.1),
-            () -> MathUtil.applyDeadband(-driver.getLeftX(), 0.1),
-            () -> MathUtil.applyDeadband(-driver.getRightY(), 0.1),
-            () -> MathUtil.applyDeadband(-driver.getRightX(), 0.1),
+            () -> -driver.getLeftY(),
+            () -> -driver.getLeftX(),
+            () -> -driver.getRightY(),
+            () -> -driver.getRightX(),
             () -> driver.rightTrigger().getAsBoolean(),
             () -> driver.leftTrigger().getAsBoolean()));
   }
