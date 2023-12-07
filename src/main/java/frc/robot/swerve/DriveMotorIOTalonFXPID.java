0bcfed70c40bf756cb66d19af552fd8e38f6da52
Hayden Heroux <haydenheroux@gmail.com>
2023-12-07 14:10:52 -0500

    feat(swerve): Implement PID algorithms.

--- src/main/java/frc/robot/swerve/DriveMotorIOTalonFXPID.java
+++ src/main/java/frc/robot/swerve/DriveMotorIOTalonFXPID.java
@@ -1,12 +1,22 @@
 package frc.robot.swerve;
 
-import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VoltageOut;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import frc.lib.hardware.CAN;
+import frc.lib.hardware.ConfigurationApplier;
 import frc.lib.math.Conversions;
 import frc.robot.Constants.Physical;
 
 /** Implements drive motor behaviors for a TalonFX. */
 public class DriveMotorIOTalonFXPID extends DriveMotorIOTalonFXBase {
+
+  private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(0, 0);
+
+  private final PIDController feedback = new PIDController(0, 0, 0);
 
   /**
    * Constructs a new TalonFX drive motor.
@@ -18,10 +28,24 @@ public class DriveMotorIOTalonFXPID extends DriveMotorIOTalonFXBase {
   }
 
   @Override
-  public void setVelocitySetpoint(double velocityMetersPerSecond) {
-    double rotationsPerSecond =
-        Conversions.General.toRotations(velocityMetersPerSecond, Physical.WHEEL_CIRCUMFERENCE);
+  public void configure() {
+    TalonFXConfiguration driveConfiguration = SwerveFactory.createDriveMotorConfig();
 
-    motor.setControl(new VelocityVoltage(rotationsPerSecond));
+    driveConfiguration.Slot0 = new Slot0Configs();
+
+    ConfigurationApplier.apply(driveConfiguration, motor);
+  }
+
+  @Override
+  public void setVelocitySetpoint(double velocityMetersPerSecond) {
+    double previousVelocityMetersPerSecond =
+        Conversions.General.toMeters(velocity.getValue(), Physical.WHEEL_CIRCUMFERENCE);
+
+    double feedforwardVolts = feedforward.calculate(velocityMetersPerSecond);
+    double feedbackVolts = feedback.calculate(previousVelocityMetersPerSecond, velocityMetersPerSecond);
+
+    double volts = feedforwardVolts + feedbackVolts;
+
+    motor.setControl(new VoltageOut(volts));
   }
 }
--- src/main/java/frc/robot/swerve/SteerMotorIOTalonFXPID.java
+++ src/main/java/frc/robot/swerve/SteerMotorIOTalonFXPID.java
@@ -1,9 +1,20 @@
 package frc.robot.swerve;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+
+import edu.wpi.first.math.controller.PIDController;
 import frc.lib.hardware.CAN;
+import frc.lib.hardware.ConfigurationApplier;
 
 /** Implements asteer motor behaviors for a TalonFX. */
 public class SteerMotorIOTalonFXPID extends SteerMotorIOTalonFXBase {
+
+  private final double kS = 0.0;
+
+  private final PIDController feedback = new PIDController(0, 0, 0);
 
   /**
    * Constructs a new TalonFX steer motor.
@@ -14,4 +25,29 @@ public class SteerMotorIOTalonFXPID extends SteerMotorIOTalonFXBase {
   public SteerMotorIOTalonFXPID(CAN motorCAN, CAN encoderCAN) {
     super(motorCAN, encoderCAN);
   }
+
+  @Override
+  public void configure() {
+    TalonFXConfiguration motorConfig = SwerveFactory.createSteerMotorConfig();
+
+    motorConfig.Slot0 = new Slot0Configs();
+
+    motorConfig.Feedback.FeedbackRemoteSensorID = 0;
+    motorConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+
+    ConfigurationApplier.apply(motorConfig, motor);
+  }
+
+  @Override
+  public void setSetpoint(double angleRotations) {
+    double previousAngleRotations = position.getValue();
+
+    double feedbackVolts = feedback.calculate(previousAngleRotations, angleRotations);
+    double feedforwardVolts = Math.signum(feedbackVolts) * kS;
+
+    double volts = feedforwardVolts + feedbackVolts;
+
+    motor.setControl(new VoltageOut(volts));
+  }
+
 }
