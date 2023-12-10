f25a537ed859853e6983c870b3fda436fd163297
Hayden Heroux <haydenheroux@gmail.com>
2023-12-10 13:45:28 -0500

    feat(swerve): Add back optimizations for ModuleIOCustom.

--- src/main/java/frc/robot/swerve/ModuleIOCustom.java
+++ src/main/java/frc/robot/swerve/ModuleIOCustom.java
@@ -3,6 +3,7 @@ package frc.robot.swerve;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
 import frc.robot.swerve.AzimuthEncoderIO.AzimuthEncoderIOValues;
 import frc.robot.swerve.DriveMotorIO.DriveMotorIOValues;
 import frc.robot.swerve.SteerMotorIO.SteerMotorValues;
@@ -76,15 +77,14 @@ public class ModuleIOCustom implements ModuleIO {
         SwerveModuleState.optimize(
             setpoint, Rotation2d.fromRotations(steerMotorValues.angleRotations));
 
-    // final double kDejitterThreshold = Units.inchesToMeters(4);
+    final double kDejitterThreshold = Units.inchesToMeters(4);
 
-    // if (Math.abs(setpoint.speedMetersPerSecond) < kDejitterThreshold) {
-    //   setpoint.angle = getState().angle;
-    // }
+    if (Math.abs(setpoint.speedMetersPerSecond) < kDejitterThreshold) {
+      setpoint.angle = getState().angle;
+    }
 
-    // //
     // https://github.com/Mechanical-Advantage/RobotCode2023/blob/bf960378bca7fe3f32c46d3d529925d960d1ff37/src/main/java/org/littletonrobotics/frc2023/subsystems/drive/Module.java#L117
-    // setpoint.speedMetersPerSecond *= setpoint.angle.minus(getState().angle).getCos();
+    setpoint.speedMetersPerSecond *= setpoint.angle.minus(getState().angle).getCos();
 
     return setpoint;
   }
